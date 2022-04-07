2b8f214a8a60ac8dc26c335ca02a21831b518b42
juldhais@gmail.com <juldhais@gmail.com>
2022-04-07 19:59:11 +0700

    Add project files.

---
+++ BenchmarkSubstringSpan.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace SubstringSpan;
+
+[MemoryDiagnoser]
+public class BenchmarkSubstringSpan
+{
+    private const string Text = "Lorem ipsum dolor sit amet.";
+    
+    [Benchmark]
+    public string Substring()
+    {
+        var substring1 = Text.Substring(6, 5);
+        var substring2 = Text.Substring(18, 3);
+        var substring3 = string.Concat(substring1, substring2);
+        return substring3;
+    }
+
+    [Benchmark]
+    public string Span()
+    {
+        var textSpan = Text.AsSpan();
+        var spanSlice1 = textSpan.Slice(6, 5);
+        var spanSlice2 = textSpan.Slice(18, 3);
+        var spanSlice3 = string.Concat(spanSlice1, spanSlice2);
+        return spanSlice3;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+using SubstringSpan;
+
+BenchmarkRunner.Run<BenchmarkSubstringSpan>();
+
+const string text = "Lorem ipsum dolor sit amet.";
+var substring1 = text.Substring(6, 5);
+var substring2 = text.Substring(18, 3);
+var substring3 = string.Concat(substring1, substring2);
+Console.WriteLine($"Substring: {substring3}");
+
+var textSpan = text.AsSpan();
+var spanSlice1 = textSpan.Slice(6, 5);
+var spanSlice2 = textSpan.Slice(18, 3);
+var spanSlice3 = string.Concat(spanSlice1, spanSlice2);
+Console.WriteLine($"Span.Slice: {spanSlice3}");
